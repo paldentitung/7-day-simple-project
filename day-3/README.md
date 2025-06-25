e2a2376a2dd96f221cd88e8d8e824278127ea2c0
Palden D. Titung <paldendorjetitung@gmail.com>
2025-06-25 21:55:31 +0545

    day-3-done

---
+++ day-3/README.md
@@ -0,0 +1,17 @@
+# Simple Calculator Project
+
+This is a basic calculator to perform tasks like addition, subtraction, multiplication, division, and more.
+
+## Features
+
+- Addition (+), Subtraction (-), Multiplication (*), Division (/)
+- Clear all (AC) and backspace (C)
+- Decimal point support
+- Percentage (%)
+
+## How to Use
+
+Open the `index.html` file in your browser.  
+Click the buttons to input numbers and operations.  
+Press `=` to see the result.
+
---
+++ day-3/index.js
@@ -0,0 +1,54 @@
+function digit(num) {
+  let userinput = document.getElementById("user-input");
+
+
+  if (num === ".") {
+
+    if (userinput.value.includes(".")) {
+      return; 
+    }
+  }
+
+  userinput.value += num; 
+}
+
+function operator(ope) {
+  let userinput = document.getElementById("user-input");
+  if (ope === "AC") {
+    userinput.value = "";
+    return;
+  }
+  if (ope === "/") {
+    userinput.value += "/";
+    return;
+  }
+  if (ope === "*") {
+    userinput.value += "*";
+    return;
+  }
+  if (ope === "-") {
+    userinput.value += "-";
+    return;
+  }
+  if (ope === "+") {
+    userinput.value += "+";
+    return;
+  }
+  if(ope === '%'){
+    userinput.value+='%'
+  }
+if (ope === 'c') {
+  userinput.value = userinput.value.slice(0, -1); 
+}
+
+}
+
+let equalBtn = document.querySelector(".IsEqual");
+equalBtn.addEventListener("click", () => {
+  let userinput = document.getElementById("user-input");
+  try {
+    userinput.value = eval(userinput.value) || 0;
+  } catch (error) {
+    userinput.value = `error ${error}`;
+  }
+});
